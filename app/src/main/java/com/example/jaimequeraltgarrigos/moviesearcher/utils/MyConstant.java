5db8530d292291bb81fc3b437c6c1ad4493b888d
Jaime <[jqueraltsound@gmail.com]>
2017-11-02 11:41:05 -0300

    Adapter setup

--- app/src/main/java/com/example/jaimequeraltgarrigos/moviesearcher/utils/MyConstant.java
+++ app/src/main/java/com/example/jaimequeraltgarrigos/moviesearcher/utils/MyConstant.java
@@ -4,9 +4,6 @@ package com.example.jaimequeraltgarrigos.moviesearcher.utils;
  * Created by jaimequeraltgarrigos on 3/12/16.
  */
 public class MyConstant {
-    public static final String DISCOVER = "discover";
-    public static final String LIVE = "live";
-    public static final String FINISHED = "finished";
 
     public static final String API_BASE_URL = "https://api.themoviedb.org";
     public static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
@@ -14,6 +11,5 @@ public class MyConstant {
     public static final String URL_SEARCH_MULTI = "3/search/multi";
 
 
-    public static final String API_KEY = "9bc3a7bc8d59c59f5ce6afa05f9a3d60";
-    public static final String MOST_POPULAR = "vote_average.desc";
+    public static final String API_KEY = "put your API key here";
 }
